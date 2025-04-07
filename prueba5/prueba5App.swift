19766a315f1c11871cae3dac3dba9998db2a70b6
Axel Tdo <axelromtad@gmail.com>
2025-04-07 17:27:47 -0600

    Initial Commit

---
+++ prueba5/prueba5App.swift
@@ -0,0 +1,17 @@
+//
+//  prueba5App.swift
+//  prueba5
+//
+//  Created by axel tadeo on 07/04/25.
+//
+
+import SwiftUI
+
+@main
+struct prueba5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
