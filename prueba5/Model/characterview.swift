81a9c0352b7b2e1844e3cd38e4134fcea8830916
Axel Tdo <axelromtad@gmail.com>
2025-04-07 17:49:02 -0600

    Este es mi segundo commit

---
+++ prueba5/Model/characterview.swift
@@ -0,0 +1,24 @@
+
+//
+//  Untitled.swift
+//  prueba5
+//
+//  Created by axel tadeo on 07/04/25.
+//
+import SwiftUI
+
+struct characterview: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, git!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    characterview()
+}
